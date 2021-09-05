8e98343245274ee2c530aa9799d96a2d5385eed0
shehla63 <68810621+shehla63@users.noreply.github.com>
2021-09-05 07:50:24 -0700

    Database Application using CRUD operations

---
+++ Student_Databse_App_Simple Task/Student_Databse_App/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Student_Databse_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentList = new System.Windows.Forms.ListBox();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.city = new System.Windows.Forms.TextBox();
+            this.state = new System.Windows.Forms.TextBox();
+            this.nationality = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // studentList
+            // 
+            this.studentList.FormattingEnabled = true;
+            this.studentList.Location = new System.Drawing.Point(32, 49);
+            this.studentList.Name = "studentList";
+            this.studentList.Size = new System.Drawing.Size(178, 316);
+            this.studentList.TabIndex = 0;
+            this.studentList.SelectedIndexChanged += new System.EventHandler(this.studentList_SelectedIndexChanged);
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(260, 48);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(269, 20);
+            this.firstName.TabIndex = 1;
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(260, 91);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(269, 20);
+            this.lastName.TabIndex = 2;
+            // 
+            // city
+            // 
+            this.city.Location = new System.Drawing.Point(260, 127);
+            this.city.Name = "city";
+            this.city.Size = new System.Drawing.Size(269, 20);
+            this.city.TabIndex = 3;
+            // 
+            // state
+            // 
+            this.state.Location = new System.Drawing.Point(260, 165);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(269, 20);
+            this.state.TabIndex = 4;
+            // 
+            // nationality
+            // 
+            this.nationality.Location = new System.Drawing.Point(260, 211);
+            this.nationality.Name = "nationality";
+            this.nationality.Size = new System.Drawing.Size(269, 20);
+            this.nationality.TabIndex = 6;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(261, 270);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(120, 41);
+            this.addButton.TabIndex = 7;
+            this.addButton.Text = "Add new Record";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(409, 270);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(120, 41);
+            this.updateButton.TabIndex = 8;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(261, 322);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(120, 41);
+            this.deleteButton.TabIndex = 9;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(409, 322);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(120, 41);
+            this.clearButton.TabIndex = 10;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 415);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.nationality);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.city);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.studentList);
+            this.Name = "Form1";
+            this.Text = "Noman_Aijaz_Student_Management_App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox studentList;
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.TextBox city;
+        private System.Windows.Forms.TextBox state;
+        private System.Windows.Forms.TextBox nationality;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Student_Databse_App_Simple Task/Student_Databse_App/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace Student_Databse_App
+{
+    public partial class Form1 : Form
+    {
+        static String connectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString;
+
+            SqlCommand command = new SqlCommand();
+            connection.Open();
+            command.Connection = connection;
+    
+            command.CommandText = "SELECT * FROM Student_Table";
+            SqlDataReader reader=command.ExecuteReader();
+
+            while (reader.Read()) {
+
+                studentList.Items.Add(reader.GetString(0));
+            }
+            reader.Close();
+            connection.Close();
+            
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            String fname, lname, student_city, student_state, student_nationality;
+            
+            fname = firstName.Text;
+            lname = lastName.Text;
+            student_city = city.Text;
+            student_state = state.Text;
+            student_nationality = nationality.Text;
+            foreach (String item in studentList.Items) {
+                if (item.ToString().Equals(fname)) {
+                    MessageBox.Show("Please Select Other First name");
+                    return;
+                }
+            }
+            if (fname.Length == 0 || lname.Length == 0 || student_city.Length == 0 || student_state.Length == 0 || student_nationality.Length == 0) {
+                MessageBox.Show("All fields required!");
+                return;
+            }
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString;
+
+            SqlCommand command = new SqlCommand();
+            connection.Open();
+            command.Connection = connection;
+
+            command.CommandText = "INSERT into Student_Table(FirstName,LastName,City,State,Nationality) VALUES('"+fname+"','"+lname+"','"+student_city+"','"+student_state+"','"+student_nationality+"')";
+            command.ExecuteNonQuery();
+            connection.Close();
+            studentList.Items.Add(fname);
+            MessageBox.Show("Record Added!");
+            ClearFileds();
+            
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            String fname, lname, student_city, student_state, student_nationality;
+
+            fname = firstName.Text;
+            lname = lastName.Text;
+            student_city = city.Text;
+            student_state = state.Text;
+            student_nationality = nationality.Text;
+
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString;
+
+            SqlCommand command = new SqlCommand();
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = "UPDATE Student_Table SET FirstName='" + fname + "',LastName='" + lname + "',City='" + student_city + "',State='" + student_state + "',Nationality='" + student_nationality +"' WHERE FirstName='"+studentList.SelectedItem.ToString()+"'";
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            studentList.Items.Clear();
+            command = new SqlCommand();
+            connection.Open();
+            command.Connection = connection;
+            
+            command.CommandText = "SELECT * FROM Student_Table";
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+                studentList.Items.Add(reader.GetString(0));
+            }
+            reader.Close();
+            connection.Close();
+            MessageBox.Show("Record Updated!");
+        
+        }
+   
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString;
+
+            SqlCommand command = new SqlCommand();
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = "DELETE FROM Student_Table WHERE FirstName='" + studentList.SelectedItem.ToString() + "'";
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            studentList.Items.Clear();
+            command = new SqlCommand();
+            connection.Open();
+            command.Connection = connection;
+
+            command.CommandText = "SELECT * FROM Student_Table";
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+                studentList.Items.Add(reader.GetString(0));
+            }
+            reader.Close();
+            connection.Close();
+            MessageBox.Show("Record DELETED!");
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            ClearFileds();
+        }
+        public void ClearFileds() {
+
+            firstName.Clear();
+            lastName.Clear();
+            state.Clear();
+            nationality.Clear();
+            city.Clear();
+        }
+
+        private void studentList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString;
+
+            SqlCommand command = new SqlCommand();
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = "SELECT * FROM Student_Table WHERE FirstName='"+studentList.SelectedItem.ToString()+"'";
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+                firstName.Text=reader.GetString(0);
+                lastName.Text= reader.GetString(1);
+                city.Text = reader.GetString(2);
+                state.Text = reader.GetString(3);
+                nationality.Text = reader.GetString(4);
+            }
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
